7f3a6b455228bb08396eb589baf4487a0e2e6895
Paweł Piskorz <pawel@arabel.la>
2017-04-21 23:25:52 +0200

    Update example component and test

--- src/MyComponent.test.js
+++ src/MyComponent.test.js
@@ -7,10 +7,19 @@ describe('<MyComponent />', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<MyComponent />);
+    wrapper = shallow(<MyComponent text="TEXT" />);
   });
 
   it('renders div', () => {
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render TEXT in p', () => {
+    expect(wrapper.find('p').text()).toBe('TEXT');
+  });
+
+  it('should render DEFAULT_TEXT in p', () => {
+    wrapper = shallow(<MyComponent />);
+    expect(wrapper.find('p').text()).toBe('DEFAULT_TEXT');
   });
 });
