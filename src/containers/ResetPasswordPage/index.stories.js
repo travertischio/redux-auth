4e25bdd019405db07e33ec272fcfda2a3ab8819d
Andrzej Dybionka <andrzej@arabel.la>
2017-09-14 14:59:04 +0200

    Fixed linter errors

--- src/containers/ResetPasswordPage/index.stories.js
+++ src/containers/ResetPasswordPage/index.stories.js
@@ -5,11 +5,11 @@ import ResetPasswordPage from './';
 const resetPasswordToken = 'MQ-NG5uLTI5Yjc3ZGZjZTM3NTQ2YTI5YTY3';
 
 storiesOf('Reset Password Page', module)
-  .add('with valid token', () => <ResetPasswordPage
+  .add('with valid token', () => (<ResetPasswordPage
     isAuthenticated
     routeParams={{ resetPasswordToken }}
-  />)
-  .add('with invalid token', () => <ResetPasswordPage
+  />))
+  .add('with invalid token', () => (<ResetPasswordPage
     isAuthenticated={false}
     routeParams={{ resetPasswordToken: 'NOT_VALID_TOKEN' }}
-  />);
+  />));
